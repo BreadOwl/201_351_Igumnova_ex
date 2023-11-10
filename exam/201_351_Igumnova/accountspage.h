69d2b41ff6e81ce326dbf15ebd45a9d3ce5fdaac
BreadOwl <igumnova.karina@yandex.ru>
2023-11-10 18:05:07 +0300

    Добавлен пин-код

--- exam/201_351_Igumnova/accountspage.h
+++ exam/201_351_Igumnova/accountspage.h
@@ -1,11 +1,42 @@
 #ifndef ACCOUNTSPAGE_H
 #define ACCOUNTSPAGE_H
 
+#pragma once
 
-class AccountsPage
+#include <QtWidgets>
+#include <QVector>
+
+// Создаем структуру счета
+struct Account {
+    int id;
+    double amount;
+    QDate date;
+    QString color;
+};
+
+class AccountsPage: public QWidget
 {
+    Q_OBJECT
 public:
-    AccountsPage();
+    AccountsPage(QWidget *parent = nullptr);
+
+private slots:
+    void previousAccount();
+    void nextAccount();
+
+signals:
+    void newWin();
+
+private:
+    QVector<Account> accounts;
+    int currentIndex;
+    QLabel *idLabel;
+    QLabel *amountLabel;
+    QLabel *dateLabel;
+    QPushButton *previousButton;
+    QPushButton *nextButton;
+
+    void loadAccount(int index);
 };
 
 #endif // ACCOUNTSPAGE_H
--- exam/201_351_Igumnova/mainwindow.cpp
+++ exam/201_351_Igumnova/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "accountspage.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    newWindow = new AccountsPage;
+    ui->error->setVisible(false);
+    ui->enterPass->setEchoMode(QLineEdit::Password);
+    connect(newWindow, &AccountsPage::newWin, this ,&MainWindow::show);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +18,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_enterBut_clicked()
+{
+    QString code = "1234";
+    if (ui->enterPass->text() == code)
+    {
+        this->close();
+        newWindow->show();
+    }
+    else
+    {
+        ui->error->setVisible(true);
+    }
+
+}
